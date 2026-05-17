0ab0cdce17ce801ca39774f679ca1cf867f2cf45
heytimmyrow-sudo <heytimmyrow@gmail.com>
2026-05-17 16:56:52 -0400

    Widen Threadmail mobile reply field

--- games/threadmail-sw.js
+++ games/threadmail-sw.js
@@ -1,12 +1,12 @@
-const THREADMAIL_CACHE = "threadmail-pwa-v23";
+const THREADMAIL_CACHE = "threadmail-pwa-v24";
 const THREADMAIL_ASSETS = [
   "./threadmail.html",
-  "./threadmail.css?v=23",
-  "./threadmail.webmanifest?v=23",
+  "./threadmail.css?v=24",
+  "./threadmail.webmanifest?v=24",
   "./threadmail-icon-192.png",
   "./threadmail-icon-512.png",
   "../style.css",
-  "../js/threadmail.js?v=23"
+  "../js/threadmail.js?v=24"
 ];
 
 self.addEventListener("install", (event) => {
