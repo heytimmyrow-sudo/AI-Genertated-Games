8827b1bc152d7b02b12e177e0ad17ed7c6e66aed
heytimmyrow-sudo <heytimmyrow@gmail.com>
2026-05-17 17:05:48 -0400

    Move ThreadAI action first

--- games/threadmail-sw.js
+++ games/threadmail-sw.js
@@ -1,12 +1,12 @@
-const THREADMAIL_CACHE = "threadmail-pwa-v25";
+const THREADMAIL_CACHE = "threadmail-pwa-v26";
 const THREADMAIL_ASSETS = [
   "./threadmail.html",
-  "./threadmail.css?v=25",
-  "./threadmail.webmanifest?v=25",
+  "./threadmail.css?v=26",
+  "./threadmail.webmanifest?v=26",
   "./threadmail-icon-192.png",
   "./threadmail-icon-512.png",
   "../style.css",
-  "../js/threadmail.js?v=25"
+  "../js/threadmail.js?v=26"
 ];
 
 self.addEventListener("install", (event) => {
