27d00db01346b2943abe0266bb6d2175cc580c06
heytimmyrow-sudo <heytimmyrow@gmail.com>
2026-05-24 13:54:31 -0400

    Enlarge Threadmail video calls on mobile

--- games/threadmail-sw.js
+++ games/threadmail-sw.js
@@ -1,12 +1,12 @@
-const THREADMAIL_CACHE = "threadmail-pwa-v47";
+const THREADMAIL_CACHE = "threadmail-pwa-v48";
 const THREADMAIL_ASSETS = [
   "./threadmail.html",
-  "./threadmail.css?v=47",
-  "./threadmail.webmanifest?v=47",
+  "./threadmail.css?v=48",
+  "./threadmail.webmanifest?v=48",
   "./threadmail-icon-192.png",
   "./threadmail-icon-512.png",
   "../style.css",
-  "../js/threadmail.js?v=47"
+  "../js/threadmail.js?v=48"
 ];
 
 self.addEventListener("install", (event) => {
